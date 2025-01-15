ca7f2fb33da63843b9cf379b70215f5a74c90304
vvmria <mariavalcarcelruiz01@alumnos.cei.es>
2025-01-15 13:19:00 +0100

    13.18

--- assets/js/galeria.js
+++ assets/js/galeria.js
@@ -1,25 +1,9 @@
-// Obtener todas las imágenes
-const images = document.querySelectorAll('.portfolio__img');
+// seleccionamos el botón y el menú
+const navToggle = document.getElementById('nav-toggle');
+const navMenu = document.getElementById('nav-menu');
 
-// Crear el lightbox (modal)
-const lightbox = document.createElement('div');
-lightbox.id = 'lightbox';
-document.body.appendChild(lightbox);
 
-// Crear la imagen dentro del lightbox
-const lightboxImage = document.createElement('img');
-lightbox.appendChild(lightboxImage);
-
-// Agregar evento click a las imágenes
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        lightboxImage.src = image.src;  // Asignar la imagen seleccionada al lightbox
-        lightbox.style.display = 'flex';  // Mostrar el lightbox
-    });
+// añadimos el evento click para mostrar/ocultar el menú
+navToggle.addEventListener('click', () => {
+    navMenu.classList.toggle('active');
 });
-
-// Cerrar el lightbox cuando se hace clic fuera de la imagen
-lightbox.addEventListener('click', () => {
-    lightbox.style.display = 'none';  // Ocultar el lightbox
-});
-
